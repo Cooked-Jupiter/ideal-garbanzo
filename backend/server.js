80925a86d8e94e294dabd1b6cbff2568187936d6
Kohl <kohlk14@gmail.com>
2021-10-11 05:54:25 -0600

    Added eslint, added the backend and starting to add communication between the two

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-    console.log(`Serve at https://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
 });
